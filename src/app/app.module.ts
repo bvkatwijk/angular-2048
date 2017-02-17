a7fcd3d42ca0094a57ccd31d9f173f7a51e9dda6
bvkatwijk <BorisK@topdesk.com>
2017-02-17 13:32:48 +0100

    Generated Background component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './components/background/background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule,
