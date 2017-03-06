fc3464cd8f55fa88850f8a9631d8ea3980a703b0
bvkatwijk <BorisK@topdesk.com>
2017-03-06 10:31:07 +0100

    Added test that app should display a board

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,26 +1,37 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { TestBed, async, ComponentFixtureAutoDetect } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/board/board.component'
 import { RowComponent } from './components/row/row.component'
 
 describe('AppComponent', () => {
-  beforeEach(() => {
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent,
         BoardComponent,
         RowComponent,
       ],
+      providers: [
+        { provide: ComponentFixtureAutoDetect, useValue: true }
+      ]
     });
     TestBed.compileComponents();
-  });
+  }));
 
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
+  it('should have a board', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(fixture.debugElement.query(By.css('app-board'))).toBeTruthy();
+  });
 
 });
