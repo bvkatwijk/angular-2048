d919e63af3b4c0d26d3fa2e113a6d03b0ee0f4d5
bvkatwijk <BorisK@topdesk.com>
2017-03-07 13:21:31 +0100

    Added rightGrid to merge class

--- src/app/components/merge/merge.ts
+++ src/app/components/merge/merge.ts
@@ -1,11 +1,11 @@
 export class Merge {
 
     static left(rowSource: number[]) {
-        var row =  rowSource.filter(val => val != 0);
+        var row = rowSource.filter(val => val != 0);
         var result: number[] = [];
         row.filter(val => val != 0)
-        for(var i = 0; i < row.length; i++) {
-            if(row[i + 1] && row[i] == row[i + 1]) {
+        for (var i = 0; i < row.length; i++) {
+            if (row[i + 1] && row[i] == row[i + 1]) {
                 result.push(row[i] * 2);
                 i++;
             } else if (row[i] != 0) {
@@ -23,6 +23,14 @@ export class Merge {
 
     static right(row: number[]) {
         return Merge.left(row.reverse()).reverse();
+    }
+
+    static rightGrid(grid: number[][]) {
+        var result: number[][] = [];
+        for (var i = 0; i < grid.length; i++) {
+            result.push(Merge.right(grid[i]));
+        }
+        return result;
     }
 
 }
